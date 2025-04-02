b65d01525f42d8241344b140c794680714beb5ea
MikelNoppo <mikelnoppo03@gmail.com>
2025-04-02 13:32:32 +0800

    enhance(auth): add helmet for security headers

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
+  app.use(helmet());
 
   await app.listen(process.env.PORT);
 }
