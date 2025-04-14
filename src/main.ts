1b8c24b3f76d9dc03adf766f906d38c95dad2737
MikelNoppo <mikelnoppo03@gmail.com>
2025-04-14 18:44:24 +0800

    feat(app): setup app CORS origin and file access

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser'; 
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('API documentation for the application')
@@ -17,6 +18,20 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
+
+  app.useStaticAssets(join(__dirname, '..', '..', 'uploads'), { 
+    prefix: '/uploads/',
+    setHeaders: (res) => {
+      res.set('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+      res.set('Access-Control-Allow-Methods', 'GET');
+    }, 
+  });
+  
   app.use(helmet());
   app.use(cookieParser()); 
 
