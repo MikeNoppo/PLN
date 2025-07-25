c00c9bf680dae5be480927e1d6c854c3837ba031
MikelNoppo <mikelnoppo03@gmail.com>
2025-07-25 15:43:32 +0800

    fix(app-update): correct indentation and update download URL in getLatestVersion method

--- src/app-update/app-update.controller.ts
+++ src/app-update/app-update.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+  import { Controller, Get } from '@nestjs/common';
 
-@Controller('app-update')
-export class AppUpdateController {
+  @Controller('app-update')
+  export class AppUpdateController {
 
-  @Get('latest')
-  getLatestVersion(){
-    return {
-      version_name : '1.0.2',
-      version_code: 2,
-      release_notes: 'Updated coook',
-      download_url: 'http://103.59.95.96:3000/uploads/app-release.apk',
+    @Get('latest')
+    getLatestVersion(){
+      return {
+        version_name : '1.0.2',
+        version_code: 2,
+        release_notes: 'Updated coook',
+        download_url: 'http://103.59.95.96:3000/uploads/apps/app-release.apk',
+      }
     }
   }
-}
