7f975d648d2c427ae0603d6fcdb8161aa0a977e8
MikelNoppo <mikelnoppo03@gmail.com>
2025-07-25 15:32:41 +0800

    feat(app-update): update latest version details to 1.0.2 with new release notes

--- src/app-update/app-update.controller.ts
+++ src/app-update/app-update.controller.ts
@@ -6,9 +6,9 @@ export class AppUpdateController {
   @Get('latest')
   getLatestVersion(){
     return {
-      version_name : '1.0.1',
-      version_code: 1,
-      release_notes: 'Initial release',
+      version_name : '1.0.2',
+      version_code: 2,
+      release_notes: 'Updated coook',
       download_url: 'http://103.59.95.96:3000/uploads/app-release.apk',
     }
   }
