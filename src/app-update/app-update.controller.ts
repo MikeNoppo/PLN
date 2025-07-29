90850125a6cfffdafedd1b2221f0a3bd71a5821d
MikelNoppo <mikelnoppo03@gmail.com>
2025-07-30 01:05:49 +0800

    Update App

--- src/app-update/app-update.controller.ts
+++ src/app-update/app-update.controller.ts
@@ -6,9 +6,9 @@
     @Get('latest')
     getLatestVersion(){
       return {
-        version_name : '1.0.2',
-        version_code: 2,
-        release_notes: 'Updated coook',
+        version_name : '1.0.3',
+        version_code: 3,
+        release_notes: 'Updated upload image from gallery',
         download_url: 'http://103.59.95.96:3000/uploads/apps/app-release.apk',
       }
     }
