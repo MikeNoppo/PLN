2a9a3858982714cd1cbb665aabbc70df4236da45
MikelNoppo <mikelnoppo03@gmail.com>
2025-08-06 03:53:40 +0800

    fix(app-update): revert version to 1.0.3 and version code to 3

--- src/app-update/app-update.controller.ts
+++ src/app-update/app-update.controller.ts
@@ -6,8 +6,8 @@
     @Get('latest')
     getLatestVersion(){
       return {
-        version_name : '1.0.4',
-        version_code: 4,
+        version_name : '1.0.3',
+        version_code: 3,
         release_notes: 'Menambahkan fitur unggah foto petugas pada laporan Yantek dan Penyambungan',
         download_url: 'https://trackmel-pln.com/uploads/apps/app-release.apk',
       }
