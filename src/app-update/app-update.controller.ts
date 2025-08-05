ce3210570299091ca9c53588b58916fb4215384b
MikeNoppo <mikelnoppo03@gmail.com>
2025-08-05 15:23:36 +0800

    feat(app-update): update latest version details to 1.0.4 with new DNS and download URL

--- src/app-update/app-update.controller.ts
+++ src/app-update/app-update.controller.ts
@@ -6,10 +6,10 @@
     @Get('latest')
     getLatestVersion(){
       return {
-        version_name : '1.0.3',
-        version_code: 3,
-        release_notes: 'Updated upload image from gallery',
-        download_url: 'http://103.59.95.96:3000/uploads/apps/app-release.apk',
+        version_name : '1.0.4',
+        version_code: 4,
+        release_notes: 'Update API call to new DNS',
+        download_url: 'https://trackmel-pln.com/uploads/apps/app-release.apk',
       }
     }
   }
