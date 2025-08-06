9cdadc8514714df9a6389df6014497d978ef40e5
MikelNoppo <mikelnoppo03@gmail.com>
2025-08-06 11:04:25 +0800

    feat(app-update): update latest version to 1.0.4 with new release notes and download URL

--- src/app-update/app-update.controller.ts
+++ src/app-update/app-update.controller.ts
@@ -6,9 +6,9 @@
     @Get('latest')
     getLatestVersion(){
       return {
-        version_name : '1.0.3',
-        version_code: 3,
-        release_notes: 'Menambahkan fitur unggah foto petugas pada laporan Yantek dan Penyambungan',
+        version_name : '1.0.4',
+        version_code: 4,
+        release_notes: 'Menambahkan foto petugas pada halaman laporan selesai admin',
         download_url: 'https://trackmel-pln.com/uploads/apps/app-release.apk',
       }
     }
