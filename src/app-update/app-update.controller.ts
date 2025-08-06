62f1eb618ec08e0db9db70cd5c72ba970c7a65a3
MikelNoppo <mikelnoppo03@gmail.com>
2025-08-06 15:26:57 +0800

    feat(app-update): update version to 1.0.5 with new release notes and download URL

--- src/app-update/app-update.controller.ts
+++ src/app-update/app-update.controller.ts
@@ -6,9 +6,9 @@
     @Get('latest')
     getLatestVersion(){
       return {
-        version_name : '1.0.4',
-        version_code: 4,
-        release_notes: 'Menambahkan foto petugas pada halaman laporan selesai admin',
+        version_name : '1.0.5',
+        version_code: 5,
+        release_notes: 'Memperbaiki Masalah google Maps, dan upl, menambahkan pemilihan sumber foto di kedua petugas Yantek dan petugas penyambungan.',
         download_url: 'https://trackmel-pln.com/uploads/apps/app-release.apk',
       }
     }
