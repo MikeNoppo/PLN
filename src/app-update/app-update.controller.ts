80c6b3d4cdb5ab36a41643dd92b993cffa41e1b2
MikelNoppo <mikelnoppo03@gmail.com>
2025-08-08 13:41:16 +0800

    feat(app-update): update latest version to 1.0.6 with new release notes

--- src/app-update/app-update.controller.ts
+++ src/app-update/app-update.controller.ts
@@ -6,9 +6,9 @@
     @Get('latest')
     getLatestVersion(){
       return {
-        version_name : '1.0.5',
-        version_code: 5,
-        release_notes: 'Memperbaiki Masalah google Maps, dan upl, menambahkan pemilihan sumber foto di kedua petugas Yantek dan petugas penyambungan.',
+        version_name : '1.0.6',
+        version_code: 6,
+        release_notes: 'memperbaiki form input pulsa dan stand meter',
         download_url: 'https://trackmel-pln.com/uploads/apps/app-release.apk',
       }
     }
