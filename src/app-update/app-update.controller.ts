617a3ed0e672f9ec2070bc8e92967969e3169ac7
MikelNoppo <mikelnoppo03@gmail.com>
2025-08-14 19:51:31 +0800

    feat: update latest version details to 1.0.7 with new release notes

--- src/app-update/app-update.controller.ts
+++ src/app-update/app-update.controller.ts
@@ -5,9 +5,9 @@ export class AppUpdateController {
   @Get('latest')
   getLatestVersion() {
     return {
-      version_name: '1.0.6',
-      version_code: 6,
-      release_notes: 'memperbaiki form input pulsa dan stand meter',
+      version_name: '1.0.7',
+      version_code: 7,
+      release_notes: 'Memperbaiki masalah laporan dengan status DIPROSES tidak muncul di halaman petugas penyambungan',
       download_url: 'https://trackmel-pln.com/uploads/apps/app-release.apk',
     };
   }
